068f63f0b91177ce9576fd07e8508a1fa42ac81a
alena patel <alenakpatel@gmail.com>
2026-06-14 19:02:13 -0400

    new tables

---
+++ ae_playground/models/staging/stg_courses.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from {{ source('blackboard', 'course_main') }}
+
+),
+
+renamed as (
+
+    select
+        pk1          as course_key,
+        course_id,
+        course_name,
+        start_date,
+        end_date,
+        row_status   as status
+    from source
+
+)
+
+select * from renamed
